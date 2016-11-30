3df7718e16589818282bade7cf52f408e4e9cf96
abhijit1997 <swabhi47@gmail.com>
2016-12-01 03:36:26 +0530

    Parents, Brother and I left :3

---
+++ app/src/main/java/com/example/riabirthday/mom1.java
@@ -0,0 +1,13 @@
+package com.example.riabirthday;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class mom1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mom1);
+    }
+}
