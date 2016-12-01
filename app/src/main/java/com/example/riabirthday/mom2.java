207f63445b6df9f615dae1ae7152975dd3952d78
abhijit1997 <swabhi47@gmail.com>
2016-12-01 14:12:30 +0530

    Family Done. Me and Brother left.

---
+++ app/src/main/java/com/example/riabirthday/mom2.java
@@ -0,0 +1,36 @@
+package com.example.riabirthday;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class mom2 extends AppCompatActivity {
+
+    MediaPlayer ourSong;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mom2);
+        ourSong= MediaPlayer.create(mom2.this,R.raw.mustafa);
+        ourSong.start();
+
+
+
+    }
+    public void mom2 (View view){
+        Intent intent=new Intent(this,mom3.class);
+        startActivity(intent);
+
+
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        ourSong.release();
+        finish();
+    }
+}
---
+++ app/src/main/java/com/example/riabirthday/mom4.java
@@ -0,0 +1,36 @@
+package com.example.riabirthday;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class mom4 extends AppCompatActivity {
+
+    MediaPlayer ourSong;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mom4);
+        ourSong= MediaPlayer.create(mom4.this,R.raw.beggar);
+        ourSong.start();
+
+
+
+    }
+    public void mom3 (View view){
+        Intent intent=new Intent(this,chachivideo.class);
+        startActivity(intent);
+
+
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        ourSong.release();
+        finish();
+    }
+}
